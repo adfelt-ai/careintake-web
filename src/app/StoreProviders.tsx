8181742da5a6b1f3739dbe01505b05b2fa522291
Henning <henning@adfelf.com>
2026-01-21 13:38:48 +0530

    feat: Introduce Suspense for application loading and temporarily disable account and venue API calls.

--- src/app/StoreProviders.tsx
+++ src/app/StoreProviders.tsx
@@ -2,17 +2,19 @@
 
 import { Provider } from "react-redux";
 import { Toaster } from "@/components/ui/sonner";
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 import AuthProvider from "./AuthProvider";
 import { store } from "./store/store";
 
 export default function StoreProviders({ children }: { children: ReactNode }) {
   return (
     <Provider store={store}>
-      <AuthProvider>
-        {children}
-        <Toaster />
-      </AuthProvider>
+      <Suspense fallback={<div>Loading...</div>}>
+        <AuthProvider>
+          {children}
+          <Toaster />
+        </AuthProvider>
+      </Suspense>
     </Provider>
   );
 }
